4e552878d3952bb50154be279aa85f8bd1d31fb1
adefemi <hardeyfehmie24@gmail.com>
2022-12-20 12:29:01 +0000

    upsd

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
+"safe function"
 
-#import sys
 
-
-#def safe_function(fct, *args):
-#    try:
-#        result = fct(*args)
-#        return (result)
-#    except:
-#        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-#        return (None)
-    
 def safe_function(fct, *args):
-    import sys
     try:
-        result = fct(*args)
-    except Exception as i:
-        sys.stderr.write("Exception: {}\n".format(i))
-        result = None
-
-    return (result)
+        resul = fct(*args)
+        return resul
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
